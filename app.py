7a4ae838964bf277963352495e3ccd5c46ff86fc
Shubham <ivaneshubham@gmail.com>
2017-07-28 21:22:52 +0530

    Project started

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/aboutus/")
+def aboutus():
+    return render_template("aboutus.html")
+
+@app.route("/contactus/")
+def contactus():
+    return render_template("contactus.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
